501890a62c56364d0242019879a31d2b33eb0da0
Connor Peoples <connor@nouseless.tech>
2022-09-24 23:58:56 -0500

    Added auto server and user id detection for cookies

--- account/get_cookies.py
+++ account/get_cookies.py
@@ -2,6 +2,7 @@
 import requests
 import time
 import os
+from urllib.parse import urlparse, parse_qs
 
 def get_cookies():
     username = os.environ["outwar_user"]
@@ -15,7 +16,10 @@ def get_cookies():
     }
 
     login_request = requests.post(url=login_url, data=login_body, allow_redirects=False)
+    url_info = urlparse(login_request.headers["Location"])
+    query = parse_qs(url_info.query)
+
     outwar_cookies = requests.utils.dict_from_cookiejar(login_request.cookies)
-    outwar_cookies['ow_userid'] = "852454" #TODO -> Get this automatically
-    outwar_cookies['ow_serverid'] = "2" #TODO -> Get this automatically
+    outwar_cookies['ow_userid'] = query["suid"][0] 
+    outwar_cookies['ow_serverid'] = query["serverid"][0]
     return(outwar_cookies)
